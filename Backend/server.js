84ff2bed6a1a342a4d19d14d368b50e016d9cf25
s-rupert <140841484+s-rupert@users.noreply.github.com>
2025-03-13 23:18:08 +0000

    feat: server setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,14 +1,9 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const app = require('./app');
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json()); // Enable JSON parsing for POST requests
-
-// This route doesn't require authentication
-app.post('/echo', (req, res) => {
-    console.log(req.body); // Log the incoming request body
-    res.json({ received: req.body }); // Send the received data back
-});
-
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
-});
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+}
+);
